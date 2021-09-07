4b0bb2feaa5c15483a5e27767dfddb7f5de46e9c
daniel <danielglx961@gmail.com>
2021-09-07 17:45:34 +0300

    add custome hooks

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,27 +1,6 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import notesContext from "../context/notes-context";
-
-// Example of custom hook
-const useMousePosition = () => {
-    const [position, setPosition] = useState({ x: 0, y: 0 });
-    useEffect(() => {
-        const handleMouseMove = (e) => {
-            setPosition({
-                x: e.pageX,
-                y: e.pageY
-            })
-        }
-        const cleanMouseEventListener = () => {
-            console.log('clean event listener');
-            document.removeEventListener('mousemove', handleMouseMove)
-        }
-        console.log('Setting Up mouse move Event');
-        document.addEventListener('mousemove', handleMouseMove)
-        return cleanMouseEventListener;
-
-    }, [])
-    return position;
-}
+import useMousePosition from "../hooks/useMousePosition"
 
 
 const Note = function ({ title, body, index }) {
---
+++ src/hooks/useMousePosition.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+
+
+const useMousePosition = () => {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+            setPosition({
+                x: e.pageX,
+                y: e.pageY
+            })
+        }
+        const cleanMouseEventListener = () => {
+            document.removeEventListener('mousemove', handleMouseMove)
+        }
+        document.addEventListener('mousemove', handleMouseMove)
+        return cleanMouseEventListener;
+
+    }, [])
+    return position;
+}
+
+
+export { useMousePosition as default }
+
+// export default useMousePosition;
